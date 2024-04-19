625336dfc7f57aa1df637b07616cde311a65bd4a
francis_liou <francis_liou@trendmicro.com>
2024-04-19 17:50:08 +0800

    test

---
+++ playwright.config.js
@@ -0,0 +1,20 @@
+// playwright.config.js
+const { devices } = require('@playwright/test')
+
+module.exports = {
+    // Configure projects for different browsers
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] }
+        }
+    ],
+    // Directory for test files
+    testDir: './tests',
+    // Global timeout for each test
+    timeout: 90000,
+    // Number of retries for a failed test
+    retries: 1,
+    // Output test results in HTML format
+    reporter: 'html'
+}
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,12 @@
 // Import the Playwright test library
 const { test, expect } = require('@playwright/test')
 
-test.describe.configure({ timeout: 90000 })
+// test.describe.configure({ timeout: 30000 })
 
 test('Navigate to Trend Micro staging', async ({ page }) => {
     page.on('request', (request) => console.log('>>', request.method(), request.url()))
     page.on('response', (response) => console.log('<<', response.status(), response.url()))
+    await new Promise((resolve) => setTimeout(resolve, 30000))
 
     test.slow()
     // Go to the specified URL
