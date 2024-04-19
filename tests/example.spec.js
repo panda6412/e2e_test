2bc8f7a0e6008ea793e8f23f8e693bee1355ce45
francis_liou <francis_liou@trendmicro.com>
2024-04-19 12:50:48 +0800

    test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,10 +1,14 @@
 // Import the Playwright test library
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 
 test('Navigate to Trend Micro staging', async ({ page }) => {
+    page.on('request', (request) => console.log('>>', request.method(), request.url()))
+    page.on('response', (response) => console.log('<<', response.status(), response.url()))
+
+    test.slow()
     // Go to the specified URL
-    await page.goto('https://stg.idprotect.trendmicro.com/');
-    
+    await page.goto('https://stg.idprotect.trendmicro.com/', { timeout: 90000 })
+
     // Check if some expected text is present
-    await expect(page).toHaveTitle(/Trend Micro ID Protection/);
-});
+    await expect(page).toHaveTitle(/Trend Micro ID Protection/)
+})
