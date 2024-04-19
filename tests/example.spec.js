ace52469fadda8af606da762b8ac54beba6b6558
francis_liou <francis_liou@trendmicro.com>
2024-04-19 18:25:03 +0800

    test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,9 +7,8 @@ test('Navigate to Trend Micro staging', async ({ page }) => {
     page.on('request', (request) => console.log('>>', request.method(), request.url()))
     page.on('response', (response) => console.log('<<', response.status(), response.url()))
 
-    test.slow()
     // Go to the specified URL
-    await page.goto('https://stg.idprotect.trendmicro.com/')
+    await page.goto('https://stg.idprotect.trendmicro.com/', { timeout: 600000 })
 
     // Check if some expected text is present
     await expect(page).toHaveTitle(/Trend Micro ID Protection/)
